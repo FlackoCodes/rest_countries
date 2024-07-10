7022230b0a72457e5e8e2466d8a8ae549e6bf74c
Flacko <lesliejoe2k2@gmail.com>
2024-07-10 13:57:59 +0000

    refactored component

--- src/component/Content.jsx
+++ src/component/Content.jsx
@@ -1,49 +1,49 @@
-import { useEffect, useState } from "react"
-import { FaSearch } from "react-icons/fa"
-import { Link, useParams } from "react-router-dom"
+// 
+import { useEffect, useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 function Content() {
+  const [countries, setCountries] = useState([]);
+  const [searchCountry, setSearchCountry] = useState('');
 
-    const [ countries, setCountries ] = useState([])
-    const [ searchCountry, setSearchCountry ] = useState('')
-    const { id } = useParams()
-    
-  const API_ENDPOINT = 'https://restcountries.com/v3.1/all?'
+  const API_ENDPOINT = 'https://restcountries.com/v3.1/all';
 
-  const userInput = (e)=>{
-    e.preventDefault()
-    const userSearch = (e.target.value);
-    setSearchCountry(userSearch)
-  }
-  
-  async function allCountries () {
+  const handleSearchInput = (e) => {
+    setSearchCountry(e.target.value);
+  };
+
+  async function fetchAllCountries() {
     try {
-      const res = await fetch(API_ENDPOINT)
-      const data = await res.json()
-      console.log(data);
+      const res = await fetch(API_ENDPOINT);
+      const data = await res.json();
       setCountries(data);
     } catch (error) {
       console.log('error fetching data', error);
     }
   }
 
-  useEffect(()=>{
-    allCountries()
-  },[id])
+  useEffect(() => {
+    fetchAllCountries();
+  }, []);
 
+  // Filter countries based on the search input
+  const filteredCountries = countries.filter(country =>
+    (country.name.common).toLowerCase().includes(searchCountry.toLowerCase())
+  );
   return (
     <>
       <main>
         <div className="content">
-          <form  action="" method="post">
+          <form action="" method="get">
             <div className="form-container">
               <div className="input-container">
                 <input
-                 type="text"
-                 placeholder="Search for a country"
-                 value={searchCountry} 
-                 onChange={userInput}
-                  />
+                  type="text"
+                  placeholder="Search for a country"
+                  value={searchCountry}
+                  onChange={handleSearchInput}
+                />
                 <FaSearch className="search-icon" />
               </div>
             </div>
@@ -59,9 +59,9 @@ function Content() {
           </form>
         </div>
         <div className="main-content">
-          {countries.map((country) => (
+          {filteredCountries.map((country) => (
             <Link
-              to={`/country-details/${country.name.common}`} 
+              to={`/country-details/${country.name.common}`}
               key={country.cca3}
               style={{ textDecoration: 'none' }}
               className="country-card"
@@ -69,7 +69,7 @@ function Content() {
               <img src={country.flags.png} alt={`${country.name.common} flag`} />
               <h2>{country.name.common}</h2>
               <p style={style}>Population: <span>{country.population.toLocaleString()}</span></p>
-              <p style={style}>Region: <span>{country.region}</span> </p>
+              <p style={style}>Region: <span>{country.region}</span></p>
               <p className="last-p" style={style}>Capital: <span>{country.capital}</span></p>
             </Link>
           ))}
@@ -79,10 +79,10 @@ function Content() {
   );
 }
 
-export default Content
+export default Content;
 
 let style = {
   padding: '.2rem .4rem',
   color: 'hsl(200, 15%, 8%)',
   fontWeight: 600,
-}
+};
